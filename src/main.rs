68b2bf6bb4dc88f748e8fb7f92489e25f4d7c2c5
maunticat <maunticat@proton.me>
2026-09-22 06:14:10 +0300

    (refactor) performance optimizations + rust qol
    replace hash map in `creatures` with a table and refactor `main` to bits

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,95 @@
-use std::env;
+use std::{env};
+
+use core::fmt;
 
 mod quotes;
 mod creatures;
 mod cowsay;
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
+use creatures::{Creature, CREATURES};
 
-    let mut quote = crate::quotes::generate_quote();
-    let mut creature_id = "-b";
-    let creatures = crate::creatures::get_creatures();
-    
-    if args.len() > 1 {
-        if args[1] == "-v" || args[1] == "--version" {
-            println!("minsay v{}", env!("CARGO_PKG_VERSION"));
-            return;
-        } if args[1] == "-h" || args[1] == "--help" {
-            println!("usage: minsay [OPTIONS] <message>
+enum MsgType {
+    None,
+    NotAFlag(String),
+    Flag(Creature),
+    Help,
+    Version,
+}
 
-options:
-      -h, --help        display this help and exit
-      -v, --version     display version number and exit
+struct Quote(pub String);
 
-creatures: ");
-            let mut keys: Vec<&str> = creatures.keys().copied().collect();
-            keys.sort();
-            for key in keys {
-                if let Some(creature) = creatures.get(key) {
-                    println!("      {}\t\t{}", key, creature.desc);
-                }
-            }
-            return;
-        } else if creatures.contains_key(args[1].as_str()) {
-            creature_id = &args[1];
-            if args.len() > 2 {
-                quote = args[2..].join(" ");
-            }
-        } else {
-            quote = args[1..].join(" ");
-        }
+struct HelpMsg;
+
+impl MsgType {
+    fn match_mb_flags(mb_flags: Option<String>) -> Self {
+        let mb_flags = match mb_flags {
+            Some(v) => v, None => return Self::None
+        };
+
+        match &*mb_flags {
+            "-v" => Self::Version,
+            "--version" => Self::Version,
+            "-h" => Self::Help,
+            "--help" => Self::Help,
+            // Return back the string if it couldn't be parsed as a flag
+            _ => CREATURES.get(&mb_flags)
+                .map(|&c| Self::Flag(c))
+                .unwrap_or_else(|| Self::NotAFlag(mb_flags))
+        } 
     }
-    
-    let creature = creatures.get(creature_id).unwrap_or(&creatures["-b"]);
-    crate::cowsay::cowsay(&quote, creature);
+}
+
+impl Quote {
+    fn merge_or_random(first_word: Option<String>, other_words: impl Iterator<Item = String>) -> Self {
+        // Reuse first word buffer for pushing other words if it exists,
+        // otherwise allocate a random String from QUOTES
+        let mut buf = match first_word {
+            None => return Self(quotes::generate_quote()),
+            Some(s) => s
+        };
+        for word in other_words {
+            buf.push_str(&word);
+        }
+        Self(buf)
+    }
+}
+
+
+impl fmt::Display for HelpMsg {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        writeln!(f, "usage: minsay [OPTIONS] <message>")?;
+       
+        // Render creatures
+        for idx in 0..CREATURES.len() {
+            let key = CREATURES.keys()[idx];
+            let desc = CREATURES.arr[idx].desc;
+            writeln!(f, "      {}\t\t{}", key, desc)?;
+        }
+        
+        Ok(())
+    }
+}
+
+
+fn main() {
+    let mut args = env::args();
+    args.next();
+
+    // Determine message type and parse creature from flag or default if needed
+    let (creature, first_word_of_quote) = match MsgType::match_mb_flags(args.next()) {
+        MsgType::Help => {
+            println!("{}", HelpMsg);
+            return;
+        }
+        MsgType::Version => {
+            println!("minsay v{}", env!("CARGO_PKG_VERSION"));
+            return
+        }
+        MsgType::Flag(creature) => (creature, args.next()),
+        MsgType::None => (CREATURES.cat(), args.next()),
+        MsgType::NotAFlag(s) => (CREATURES.cat(), Some(s)),
+    };
+   
+    let quote = Quote::merge_or_random(first_word_of_quote, args);
+    crate::cowsay::cowsay(&quote.0, &creature);
 }
